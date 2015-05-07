aa7a0189c374a3410221fa5953d0ec04ac969a75
tortoise4race <tortoise4race@gmail.com>
2015-05-07 15:20:41 +0200

    fix bug

--- labs/scripts/spfile2pfile.sh
+++ labs/scripts/spfile2pfile.sh
@@ -5,16 +5,16 @@
 domain=`uname -n`.`awk  '/domain/ {print $2}' /etc/resolv.conf`
 sid=orcl
 
-strings $1 | sed s,'/u01/app/oracle','/export/home/ora11s/app',1 			\
-	   | sed s,"*.compatible.*","*.compatible='11.2.0.0.0'",1			\
-	   | sed '/^*.service_names/d' 							\
-	   | sed '/^*.control_files/,+1d' 						\
-	   | sed s,"^*.db_domain.*","*.db_domain='$domain'",1 				\
-	   > pfile_$1.ora
-cat >> pfile_$1.ora <<EOF
+strings $1 | gsed s,'/u01/app/oracle','/export/home/ora11s/app',1 			\
+	   | gsed s,"*.compatible.*","*.compatible='11.2.0.0.0'",1			\
+	   | gsed '/^*.service_names/d' 							\
+	   | gsed '/^*.control_files/,+1d' 						\
+	   | gsed s,"^*.db_domain.*","*.db_domain='$domain'",1 				\
+	   > pfile.ora
+cat >> pfile.ora <<EOF
 *.control_files='/export/home/ora11s/app/oradata/orcl/control01.ctl','/export/home/ora11s/app/oradata/orcl/control02.ctl'
 *.service_names='$sid.$domain'
 *.nls_language='GERMAN'
 *.nls_territory='GERMANY'
 EOF
-cat pfile_$1.ora
+cat pfile.ora
